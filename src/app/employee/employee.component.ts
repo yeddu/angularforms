79427189cab53336a9a4b5a7a5e3ca872dc28e74
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-09-11 10:37:05 -0500

    Moved the Employee form to its own component.

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'af-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
